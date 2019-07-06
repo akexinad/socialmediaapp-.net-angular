96ca943c9a88b0dda0ca1530b84b0afb496819a4
akexinad <danixeka@gmail.com>
2019-07-06 17:05:03 +1000

    11. Making our code asynchronous

--- SocialMediaApp.API/Controllers/ValuesController.cs
+++ SocialMediaApp.API/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SocialMediaApp.API.Data;
 
 namespace SocialMediaApp.API.Controllers
@@ -27,18 +28,18 @@ namespace SocialMediaApp.API.Controllers
         // GET api/values
         // IActionResult allows us to return HTTP responses to the client.
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
 
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(val => val.Id == id);
+            var value = await _context.Values.FirstOrDefaultAsync(val => val.Id == id);
 
             return Ok(value);
         }
