fa961d6a369eefb64f813c0d8fd577762a724f1b
akexinad <danixeka@gmail.com>
2019-08-10 18:45:29 +1000

    76. Using AutoMapper Part 2

--- SocialMediaApp.API/Helpers/AutoMapperProfiles.cs
+++ SocialMediaApp.API/Helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using SocialMediaApp.API.Dtos;
 using SocialMediaApp.API.Models;
@@ -8,8 +9,24 @@ namespace SocialMediaApp.API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailedDto>();
+            CreateMap<User, UserForListDto>()
+                // We need to tell automapper how to map the PhotoUrl to be the photo that isMain
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                // And we need to calculate the age from the date of birth.
+                // We created a helper in Extension.cs for this which extends from DateTime
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
 }
