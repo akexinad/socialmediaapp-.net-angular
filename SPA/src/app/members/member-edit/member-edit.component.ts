713ca2f5cf88a93c2263d97f9c9f105a5df76029
danny <danny.ceccattini@lendlease.com>
2019-08-12 09:15:30 +1000

    96. Creating the Member Edit Template part 2

--- SPA/src/app/members/member-edit/member-edit.component.ts
+++ SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '@models/user';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { AlertifyService } from '@services/alertify.service';
+
+import { User } from '@models/user';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -9,18 +13,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberEditComponent implements OnInit {
 
+  @ViewChild('editForm', { static: true }) editForm: NgForm;
   user: User;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
   ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      console.log(data);
-      
       this.user = data.userFromEditResolver;
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Updated successfully!');
+    // Reset the form with information stored in the user model
+    this.editForm.reset(this.user);
   }
 
 }
